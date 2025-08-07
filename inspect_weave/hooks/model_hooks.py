5b6a77605a512c74132c02ee5a84d3565c726808
Esther Guo <special0831qi@gmail.com>
2025-08-07 23:03:14 +0800

    update import; update  function call

--- inspect_weave/hooks/model_hooks.py
+++ inspect_weave/hooks/model_hooks.py
@@ -14,7 +14,7 @@ from inspect_viz.plot import write_png_async
 
 from inspect_viz.view.beta import scores_heatmap
 from inspect_viz import Data
-from inspect_ai.analysis.beta import evals_df
+from inspect_ai.analysis import evals_df
 from inspect_weave.config.settings_loader import SettingsLoader
 from inspect_weave.config.settings import ModelsSettings
 
@@ -88,7 +88,7 @@ class WandBModelHooks(Hooks):
 
     async def _log_scores_heatmap(self, data: RunEnd, df: pd.DataFrame) -> None:
         viz_data = Data.from_dataframe(df)
-        plot = scores_heatmap(viz_data, x="task_display_name", y="model", fill="score_headline_value")
+        plot = scores_heatmap(viz_data, task_name="task_display_name", model_name="model", score_value="score_headline_value")
 
         await self._log_image(data.run_id, plot, "scores_heatmap")
 
